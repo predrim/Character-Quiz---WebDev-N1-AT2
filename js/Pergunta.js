904dc2d8c14b3c0350db8dad42ee9f5f9e899d63
predrim <pedrofsp2@gmail.com>
2026-04-21 16:53:35 -0300

    add class for questions

---
+++ js/Pergunta.js
@@ -0,0 +1,9 @@
+export class Pergunta {
+    constructor(image, question, answers = []) {
+        this.image = image; 
+        this.question = question; 
+        this.answers = answers; 
+    }
+
+    
+}
