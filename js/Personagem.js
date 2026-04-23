b3bc98430be39d19791db91941f89d1cc19a50ed
predrim <pedrofsp2@gmail.com>
2026-04-23 20:50:38 -0300

    add final touches

--- js/Personagem.js
+++ js/Personagem.js
@@ -1,26 +1,25 @@
 export class Personagem{
     
     constructor(name, sex, color, food, virtue, profession, animal, superpower, image, info, points){
-        this.points = 0
-        
-        this.name = name
-        this.sex = sex
-        this.color = color
-        this.food = food
-        this.virtue = virtue
-        this.profession = profession
-        this.animal = animal
-        this.superpower = superpower
-        this.image = image
-        this.info = info
+        this.name = name;
+        this.sex = sex;
+        this.color = color;
+        this.food = food;
+        this.virtue = virtue;
+        this.profession = profession;
+        this.animal = animal;
+        this.superpower = superpower;
+        this.image = image;
+        this.info = info;
+        this.points = 0;
     }
 
     addPontos(){
-        this.points += 1
+        this.points += 1;
     }
 
     subPontos(){
-        this.points -= 1
+        this.points -= 1;
     }
 }
 
--- js/script.js
+++ js/script.js
@@ -3,44 +3,42 @@ import { Personagem } from "./Personagem.js";
 
 let page = 1;
 
-//variaveis para os objetos personagem
 let luffy = new Personagem("Luffy", "Masculino", "Vermelho", "Carne", "Liberdade", "Bombeiro", "Macaco", "Esticar-se", "../assets/images/characters/Luffy.png",
     "Monkey D. Luffy é o capitão de borracha dos Piratas do Chapéu de Palha que busca o lendário One Piece para se tornar o Rei dos Piratas. Conhecido por seu otimismo contagiante e apetite insaciável, ele combina uma personalidade ingênua com uma lealdade feroz, sendo capaz de desafiar qualquer poder mundial para proteger seus amigos e garantir a liberdade."
-)
+);
 let zoro = new Personagem("Zoro", "Masculino", "Verde", "Arroz", "Disciplina", "Policial", "Tigre", "Virar um dragão", "../assets/images/characters/Zoro.png",
     "Roronoa Zoro é o implacável mestre do Estilo de Três Espadas e o primeiro imediato do bando. Movido pela promessa de ser o maior espadachim do mundo, ele é definido por uma disciplina ferrenha e lealdade inabalável a Luffy, atuando como o pilar de seriedade e força bruta da tripulação."
-)
+);
 let nami = new Personagem("Nami", "Feminino", "Laranja", "Tangerina", "Prudência", "Babá", "Gato", "Disparar raios", "../assets/images/characters/Nami.png",
     "Nami é a genial navegadora dos Chapéu de Palha, cujo sonho é mapear o mundo inteiro. Além de sua inteligência estratégica, ela utiliza o Clima-Tact para controlar o clima em combate, sendo o coração compassivo que equilibra o caos do bando com pragmatismo e coragem."
-)
+);
 let usopp = new Personagem("Usopp", "Masculino", "Amarelo", "Peixe", "Bravura", "Designer Gráfico", "Camaleão", "Ter um bolso infinito", "../assets/images/characters/Usopp.png",
     "Usopp é o atirador e mestre da inventividade do bando, utilizando precisão e estratégias astutas para vencer. Embora seja o membro mais medroso, ele busca se tornar um bravo guerreiro do mar, provando que a verdadeira coragem é enfrentar o perigo mesmo quando se está morrendo de medo."
-)
+);
 let sanji = new Personagem("Sanji", "Masculino", "Azul", "Massas", "Devoção", "Cabelereiro", "Cabra", "Intangibilidade", "../assets/images/characters/Sanji.png",
     "Sanji é o mestre cuca da tripulação, um cozinheiro de elite que luta apenas com as pernas para preservar suas mãos. Estrategista brilhante e cavalheiro incorrigível, ele busca o lendário All Blue enquanto protege seus amigos com chutes poderosos e uma paixão ardente."
-)
+);
 let chopper = new Personagem("Chopper", "Masculino", "Rosa", "Algodão doce", "Inocência", "Professor", "Panda Vermelho", "Super inteligência", "../assets/images/characters/Chopper.png",
     "Tony Tony Chopper é o médico, uma rena que adquiriu consciência humana após comer a Hito Hito no Mi. De grande inocência e coração puro, ele utiliza suas transformações e conhecimentos medicinais para curar seus aliados, sonhando em um dia ser capaz de tratar todas as doenças do mundo."
-)
+);
 let robin = new Personagem("Robin", "Feminino", "Violeta", "Sanduiche", "Paciência", "Comissário de bordo", "Falcão", "Crescer membros onde quiser", "../assets/images/characters/Robin.png",
     "Nico Robin é a arqueóloga do bando e a única capaz de ler os Poneglyphs para desvendar a História Perdida. Usuária da Hana Hana no Mi, ela floresce partes do corpo em qualquer superfície. De mente fria e introspectiva, encontrou nos Chapéu de Palha o porto seguro que buscou por toda a vida."
-)
+);
 let franky = new Personagem("Franky", "Masculino", "Azul Claro", "Hamburguer", "Autenticidade", "Piloto de Avião", "Rinoceronte", "Transformar-se em armas", "../assets/images/characters/Franky.png",
     "Franky é o carpinteiro naval ciborgue que construiu e mantém o Thousand Sunny, atual SUUPER navio do bando. Extravagante e emotivo, ele protege o bando com seu corpo blindado e armas embutidas, vivendo sob o lema 'SUPER' enquanto navega rumo ao fim do mundo."
-)
+);
 let brook = new Personagem("Brook", "Masculino", "Preto", "Chá", "Alegria", "Detetive", "Girafa", "Ressuscitar", "../assets/images/characters/Brook.png",
     "Brook é o músico e esqueleto vivo que retornou à vida graças à Yomi Yomi no Mi. Combinando esgrima clássica com melodias hipnóticas, ele traz alegria à tripulação com suas piadas de caveira, enquanto busca cumprir a promessa de reencontrar a baleia Laboon."
-)
+);
 let jinbe = new Personagem("Jinbe", "Masculino", "Marrom", "Sushi e Sashimi", "Integridade", "Maquinista", "Urso", "Poder voar", "../assets/images/characters/Jinbe.png",
     "Jinbe é o honrado timoneiro e mestre do Karatê dos Homens-Peixe, atuando como o pilar de sabedoria dos Chapéu de Palha. Com força colossal e controle sobre as águas, ele dedica sua vida a ajudar Luffy a mudar o destino do mundo e promover a paz entre as raças."
-)
+);
 
 let personagens = [luffy, zoro, nami, usopp, sanji, chopper, robin, franky, brook, jinbe];
 
-//variaveis para os objetos pergunta
 let pergunta1 = new Pergunta("Você é do sexo masculino ou feminino?",
     ["Masculino", "Feminino"],
-    "../assets/images/question1.webp")
+    "../assets/images/question1.webp");
 
 let pergunta2 = new Pergunta("Qual é a sua cor favorita",
     ["Vermelho", "Verde", "Laranja", "Amarelo", "Azul", "Rosa", "Violeta", "Azul Claro", "Preto", "Marrom"],
@@ -56,7 +54,7 @@ let pergunta4 = new Pergunta("Com qual dessas virtudes você mais se identifica?
 
 let pergunta5 = new Pergunta("Com qual dessas profissões você identifica?",
     ["Bombeiro", "Policial", "Babá", "Designer Gráfico", "Cabelereiro", "Professor", "Comissário de bordo", "Piloto de Avião", "Detetive", "Maquinista"],
-    "../assets/images/question5.webp")
+    "../assets/images/question5.webp");
 
 let pergunta6 = new Pergunta("Qual desses animais você mais gosta?",
     ["Macaco", "Tigre", "Gato", "Camaleão", "Cabra", "Panda Vermelho", "Falcão", "Rinoceronte", "Girafa", "Urso"],
@@ -64,15 +62,13 @@ let pergunta6 = new Pergunta("Qual desses animais você mais gosta?",
 
 let pergunta7 = new Pergunta("Dentre esses superpoderes, qual você prefere?", ["Esticar-se", "Virar um dragão", "Disparar raios",
     "Ter um bolso infinito", "Intangibilidade", "Tranformar-se em armas", "Ressuscitar", "Poder voar", "Super inteligência", "Crescer membros onde quiser"],
-    "../assets/images/question7.webp")
+    "../assets/images/question7.webp");
 
-// variaveis para formatação da página
 let questionElement = document.querySelector(".question");
 let answersContainer = document.querySelector(".answers-container");
 let imageElement = document.querySelector(".illustration");
 let backButton = document.querySelector("#bck-btn");
 
-//eventos para o botão de voltar
 backButton.addEventListener("click", (event) => {
     if (page > 1) {
         event.preventDefault();
@@ -109,9 +105,9 @@ backButton.addEventListener("click", (event) => {
             })
         }
         page -= 1;
-        updateGame()
+        updateGame();
     }
-    return event
+    return event;
 });
 
 backButton.addEventListener("mouseover", () => {
@@ -121,36 +117,34 @@ backButton.addEventListener("mouseout", () => {
     backButton.style.scale = "1";
 })
 
-
-//função para atualizar a página
 function updateGame() {
     switch (page) {
         case 1:
-            makePage(pergunta1)
+            makePage(pergunta1);
             break;
 
         case 2:
-            makePage(pergunta2)
+            makePage(pergunta2);
             break;
 
         case 3:
-            makePage(pergunta3)
+            makePage(pergunta3);
             break;
 
         case 4:
-            makePage(pergunta4)
+            makePage(pergunta4);
             break;
 
         case 5:
-            makePage(pergunta5)
+            makePage(pergunta5);
             break;
 
         case 6:
-            makePage(pergunta6)
+            makePage(pergunta6);
             break;
 
         case 7:
-            makePage(pergunta7)
+            makePage(pergunta7);
             break;
         default:
             calculateResult();
@@ -159,11 +153,9 @@ function updateGame() {
     }
 }
 
-//função para criar os elementos da página de acordo com a pergunta atual
 function makePage(pergunta) {
         questionElement.textContent = pergunta.name;
-        imageElement.src = pergunta.image
-
+        imageElement.src = pergunta.image;
 
         if (answersContainer) answersContainer.innerHTML = "";
 
@@ -193,10 +185,9 @@ function makePage(pergunta) {
         }
 }
 
-//função para calcular o resultado do quiz
 function calculateResult() {
     let perguntas = [pergunta1, pergunta2, pergunta3, pergunta4, pergunta5, pergunta6, pergunta7];
-    let atributos = ["sex", "color", "food", "virtue", "profession", "animal", "superpower"]
+    let atributos = ["sex", "color", "food", "virtue", "profession", "animal", "superpower"];
     let match = null;
 
     personagens.forEach(personagem => personagem.points = 0);
